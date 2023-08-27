0e1bd223b2b75088a4b1e0bbcc7c218fc240b9c5
MoisesGJ <mgarciaj1802@alumno.ipn.mx>
2023-08-27 17:15:49 -0600

    Add method auth. MIddleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,24 @@
+const createHttpError = require('http-errors');
+
+const { verify } = require('../lib/jwt');
+
+const auth = (req, res, next) => {
+  try {
+    const auth = req.headers.authorization;
+
+    if (!auth) throw new createHttpError(401, 'Token required');
+
+    const token = auth.split('Bearer ', '');
+
+    const payload = verify(token);
+
+    if (!payload) throw new createHttpError(401, 'Could not verify token');
+
+    next();
+  } catch (err) {
+    console.log(err);
+    res.status(401).json({ message: 'Unauthorized' });
+  }
+};
+
+module.exports = auth;

559001b0f0c7c3697da4936272c7054c8ab4da12
MoisesGJ <mgarciaj1802@alumno.ipn.mx>
2023-08-27 18:09:29 -0600

    Fix logical returns false if token is not valid in verify

--- src/lib/jwt.js
+++ src/lib/jwt.js
@@ -5,6 +5,13 @@ const SECRET_WORD = process.env.JWT_SECRET;
 const createToken = (payload) =>
   sign(payload, SECRET_WORD, { expiresIn: '1d' });
 
-const verifyToken = (token) => verify(token, SECRET_WORD);
+const verifyToken = (token) =>
+  verify(token, SECRET_WORD, function (err) {
+    if (err) {
+      console.log(err);
+      return false;
+    }
+    return true;
+  });
 
 module.exports = { createToken, verifyToken };

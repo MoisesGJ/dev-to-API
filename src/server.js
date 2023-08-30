3db403f9467b8ff644b0d47dc78c10f948998398
MoisesGJ <mgarciaj1802@alumno.ipn.mx>
2023-08-29 22:42:52 -0600

    Add docu

--- src/server.js
+++ src/server.js
@@ -12,6 +12,15 @@ app.use(express.json());
 
 app.get('/', (req, res) => res.json({ message: 'Dev-To API | v1.0.0' }));
 
+app.get('/DOCUMENTACION', (req, res) =>
+  res
+    .status(402)
+    .json({
+      error:
+        'Para acceder a la documentación necesita pagar una subscripción mensuaul.',
+    })
+);
+
 app.use('/auth', authRouter);
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
